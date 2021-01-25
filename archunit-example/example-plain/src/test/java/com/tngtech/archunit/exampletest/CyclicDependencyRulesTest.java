05a0c683f387127e28e380ff5ef0ae5ce81c3b7a
Manfred Hanke <Manfred.Hanke@tngtech.com>
2021-01-25 01:23:10 +0100

    freeze cyclic dependency rules and regenerate store on Windows

--- archunit-example/example-plain/src/test/java/com/tngtech/archunit/exampletest/CyclicDependencyRulesTest.java
+++ archunit-example/example-plain/src/test/java/com/tngtech/archunit/exampletest/CyclicDependencyRulesTest.java
@@ -13,6 +13,7 @@ import org.junit.experimental.categories.Category;
 import static com.tngtech.archunit.base.DescribedPredicate.alwaysTrue;
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+import static com.tngtech.archunit.library.freeze.FreezingArchRule.freeze;
 
 @Category(Example.class)
 public class CyclicDependencyRulesTest {
@@ -21,68 +22,68 @@ public class CyclicDependencyRulesTest {
 
     @Test
     public void no_cycles_by_method_calls_between_slices() {
-        slices().matching("..(simplecycle).(*)..").namingSlices("$2 of $1")
-                .should().beFreeOfCycles()
+        freeze(slices().matching("..(simplecycle).(*)..").namingSlices("$2 of $1")
+                .should().beFreeOfCycles())
                 .check(classes);
     }
 
     @Test
     public void no_cycles_by_constructor_calls_between_slices() {
-        slices().matching("..(constructorcycle).(*)..").namingSlices("$2 of $1")
-                .should().beFreeOfCycles()
+        freeze(slices().matching("..(constructorcycle).(*)..").namingSlices("$2 of $1")
+                .should().beFreeOfCycles())
                 .check(classes);
     }
 
     @Test
     public void no_cycles_by_inheritance_between_slices() {
-        slices().matching("..(inheritancecycle).(*)..").namingSlices("$2 of $1")
-                .should().beFreeOfCycles()
+        freeze(slices().matching("..(inheritancecycle).(*)..").namingSlices("$2 of $1")
+                .should().beFreeOfCycles())
                 .check(classes);
     }
 
     @Test
     public void no_cycles_by_field_access_between_slices() {
-        slices().matching("..(fieldaccesscycle).(*)..").namingSlices("$2 of $1")
-                .should().beFreeOfCycles()
+        freeze(slices().matching("..(fieldaccesscycle).(*)..").namingSlices("$2 of $1")
+                .should().beFreeOfCycles())
                 .check(classes);
     }
 
     @Test
     public void no_cycles_by_member_dependencies_between_slices() {
-        slices().matching("..(membercycle).(*)..").namingSlices("$2 of $1")
-                .should().beFreeOfCycles()
+        freeze(slices().matching("..(membercycle).(*)..").namingSlices("$2 of $1")
+                .should().beFreeOfCycles())
                 .check(classes);
     }
 
     @Test
     public void no_cycles_in_simple_scenario() {
-        slices().matching("..simplescenario.(*)..").namingSlices("$1")
-                .should().beFreeOfCycles()
+        freeze(slices().matching("..simplescenario.(*)..").namingSlices("$1")
+                .should().beFreeOfCycles())
                 .check(classes);
     }
 
     @Test
     public void no_cycles_in_complex_scenario() {
-        slices().matching("..(complexcycles).(*)..").namingSlices("$2 of $1")
-                .should().beFreeOfCycles()
+        freeze(slices().matching("..(complexcycles).(*)..").namingSlices("$2 of $1")
+                .should().beFreeOfCycles())
                 .check(classes);
     }
 
     @Test
     public void no_cycles_in_complex_scenario_with_custom_ignore() {
-        slices().matching("..(complexcycles).(*)..").namingSlices("$2 of $1")
+        freeze(slices().matching("..(complexcycles).(*)..").namingSlices("$2 of $1")
                 .as("Slices of complex scenario ignoring some violations")
                 .should().beFreeOfCycles()
                 .ignoreDependency(SliceOneCallingConstructorInSliceTwoAndMethodInSliceThree.class, ClassCallingConstructorInSliceFive.class)
-                .ignoreDependency(resideInAPackage("..slice4.."), alwaysTrue())
+                .ignoreDependency(resideInAPackage("..slice4.."), alwaysTrue()))
                 .check(classes);
     }
 
     @Test
     public void no_cycles_in_freely_customized_slices() {
-        slices().assignedFrom(inComplexSliceOneOrTwo())
+        freeze(slices().assignedFrom(inComplexSliceOneOrTwo())
                 .namingSlices("$1[$2]")
-                .should().beFreeOfCycles()
+                .should().beFreeOfCycles())
                 .check(classes);
     }
 
